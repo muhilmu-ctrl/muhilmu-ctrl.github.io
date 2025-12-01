f00d24454fa694bf881f6c6629ec06f13f5aef7c
muhilmu-ctrl <mmuruga2@charlotte.edu>
2025-11-30 21:29:59 -0500

    added confirmation popup box to form

--- itis3135/client_project/scripts/contact_form.js
+++ itis3135/client_project/scripts/contact_form.js
@@ -1,11 +1,11 @@
 function validatePhoneNum(p){
     const phoneRegex = /^[0-9]{10}$/;
-    return phoneRegex.test(d);
+    return phoneRegex.test(p);
 }
 
 function validateDate(d){
-    const dateRegex = /^\d{2}-\d{2}-\d{4}$/;
-    return dateRegex.test(p);
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    return dateRegex.test(d);
 }
 
 
@@ -19,21 +19,38 @@ document.querySelector("form").addEventListener("submit",function(e){
     const eventLocation = document.getElementById("eventLocation").value.trim();
 
     let errorMsg = "";
+    const msg = `
+        <strong>First Name: </strong> ${firstName}<br>
+        <strong>Last Name: </strong> ${lastName}<br>
+        <strong>Phone Number: </strong> ${phoneNum}<br>
+        <strong>Event Date: </strong> ${eventDate}<br>
+        <strong>Event Time: </strong> ${eventTime}<br>
+        <strong>Event Location: </strong> ${eventLocation}<br>
+    `;
 
     if (!firstName || !lastName || !phoneNum || !eventDate || !eventTime || !eventLocation){
         errorMsg = "Please fill in all fields.";
-    } else if (!validatePhoneNum(p)){
+    } else if (!validatePhoneNum(phoneNum)){
         errorMsg = "Please enter a valid phone number.";
-    } else if (!validateDate(d)){
+    } else if (!validateDate(eventDate)){
         errorMsg = "Please enter a valid date.";
     }
 
     if (errorMsg){
         alert(errorMsg);
-    } else {
-        alert("Form submitted successfully!");
-        document.querySelector("form").reset;
+        return;
     }
+
+    document.getElementById("confirmText").innerHTML = msg;
+    document.getElementById("confirmationPopup").style.display = "block";
     
 });
 
+document.getElementById("confirmBtn").addEventListener("click", function(){
+    document.getElementById("confirmationPopup").style.display = "none";
+    alert("Thank you! Your information has been confirmed!");
+    document.querySelector("form").reset();
+});
+document.getElementById("editBtn").addEventListener("click", function(){
+    document.getElementById("confirmationPopup").style.display = "none";
+});
